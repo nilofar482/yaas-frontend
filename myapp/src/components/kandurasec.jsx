b8f0b5b1cbda34329ba611e7648ea5a449a2822f
nilofar482 <Nilofar2001k@gmail.com>
2026-04-27 19:24:52 +0530

    next

--- myapp/src/components/kandurasec.jsx
+++ myapp/src/components/kandurasec.jsx
@@ -44,6 +44,7 @@ function KanduraSlider() {
 
   return (
     <div>
+    <p className='top-sales'>Premium Kandura Collection</p>
       <div className="split-wrapper">
 
         {/* LEFT SIDE: VIDEO */}
