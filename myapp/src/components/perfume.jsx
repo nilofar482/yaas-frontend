0cd43050089df6e3afcd401165bc6e9feace626b
nilofar482 <Nilofar2001k@gmail.com>
2026-06-04 11:31:40 +0530

    scroll

--- myapp/src/components/perfume.jsx
+++ myapp/src/components/perfume.jsx
@@ -1,12 +1,10 @@
 import axios from "axios";
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState } from "react";
 import PopupCart from "./popupcart";
 
 function Perfumes() {
   const [product, setproduct] = useState([]);
   const [selectedProduct, setSelectedProduct] = useState(null);
-
-  const scrollRef = useRef();
 
   useEffect(() => {
     axios
@@ -15,72 +13,56 @@ function Perfumes() {
       .catch((err) => console.log(err));
   }, []);
 
-  const scrollLeft = () => {
-    scrollRef.current.scrollBy({ left: -300, behavior: "smooth" });
-  };
-
-  const scrollRight = () => {
-    scrollRef.current.scrollBy({ left: 300, behavior: "smooth" });
-  };
-
   return (
-    <div>
-      <div className="scroll_wrapper">
+    <div className="product_list">
+      {product.map((s) => {
+        const totalStock = s.colors?.reduce((total, color) => {
+          return (
+            total +
+            (color.sizes?.reduce(
+              (sum, size) => sum + (size.stock || 0),
+              0
+            ) || 0)
+          );
+        }, 0);
 
-        <button className="scroll_btn left" onClick={scrollLeft}>‹</button>
-
-        <div className="product_list" ref={scrollRef}>
-          {product.map((s) => {
-
-            const totalStock = s.colors?.reduce((total, color) => {
-              return total + (color.sizes?.reduce((sum, size) => sum + (size.stock || 0), 0) || 0);
-            }, 0);
-
-            return (
-              <div key={s.id} className="product-card1">
-
-                <div className="img-box">
-                  {s.colors && s.colors.length > 0 ? (
-                    <>
-                      <img
-                        src={`https://api.yaasgents.com${s.colors[0].image1}`}
-                        alt={s.name}
-                      />
-                      {s.colors[0].image2 && (
-                        <img
-                          src={`https://api.yaasgents.com${s.colors[0].image2}`}
-                          alt={s.name}
-                          className="hover-img"
-                        />
-                      )}
-                    </>
-                  ) : (
-                    <p>No Image</p>
+        return (
+          <div key={s.id} className="product-card1">
+            <div className="img-box">
+              {s.colors && s.colors.length > 0 ? (
+                <>
+                  <img
+                    src={`https://api.yaasgents.com${s.colors[0].image1}`}
+                    alt={s.name}
+                  />
+                  {s.colors[0].image2 && (
+                    <img
+                      src={`https://api.yaasgents.com${s.colors[0].image2}`}
+                      alt={s.name}
+                      className="hover-img"
+                    />
                   )}
-                </div>
+                </>
+              ) : (
+                <p>No Image</p>
+              )}
+            </div>
 
-                {/* SOLD OUT */}
-                {totalStock === 0 && (
-                  <div className="sold_out">Out of Stock</div>
-                )}
+            {totalStock === 0 && (
+              <div className="sold_out">Out of Stock</div>
+            )}
 
+            <div className="product_name">{s.name}</div>
+            <div className="price">AED {s.price}</div>
 
-                <div className="product_name">{s.name}</div>
-
-                <div className="price">AED {s.price}</div>
-                <div className="carticon">
-                  <button onClick={() => setSelectedProduct(s)}>
-                    buy now
-                  </button>
-                </div>
-
-              </div>
-            );
-          })}
-        </div>
-
-        <button className="scroll_btn right" onClick={scrollRight}>›</button>
-      </div>
+            <div className="carticon">
+              <button onClick={() => setSelectedProduct(s)}>
+                Buy Now
+              </button>
+            </div>
+          </div>
+        );
+      })}
 
       {selectedProduct && (
         <PopupCart
